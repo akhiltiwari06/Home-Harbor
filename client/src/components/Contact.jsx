cb9a91d3d3182c36d3dc3ba1f9841d17c010ed7b
akhiltiwari06 <akhiltiwari2806@gmail.com>
2024-03-01 16:15:25 +0530

    completed seacrh api route

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,23 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// export default function Contact({ listing }) {
-//   const [landlord, setLandlord] = useState(null);
-
-//   useEffect(() => {
-//     const fetchLandlord = async () => {
-//       try {
-//         const res = await fetch(`/api/user/${listing.userRef}`);
-//         const data = await res.json();
-//         setLandlord(data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchLandlord();
-//   }, [listing.userRef]);
-//   return <div></div>;
-// }
-
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
