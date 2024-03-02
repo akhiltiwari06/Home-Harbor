9c6b8918ad0f63e0e481dd9d4858b1316096bcab
akhiltiwari06 <akhiltiwari2806@gmail.com>
2024-03-03 01:10:55 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,26 @@
-// import React from 'react'
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Home Harbor
+      </h1>
+      <p className="mb-4 text-slate-700">
+        HomeHarbor is here to help you find, sell, or rent your dream home in
+        the best neighborhoods. Our experienced team works hard to make buying
+        or selling homes easy and stress-free for you.
+      </p>
+      <p className="mb-4 text-slate-700">
+        We are all about helping you reach your goals in real estate with smart
+        advice, personal attention, and deep knowledge of the local housing
+        market. Whether you want to buy, sell, or rent, we are with you every
+        step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our agents know the real estate world inside and out, and we are
+        committed to giving you the best service possible. We believe finding or
+        selling a home should be exciting and rewarding, and we are here to make
+        sure that happens for every client.
+      </p>
+    </div>
+  );
 }
